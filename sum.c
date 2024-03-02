0f3b4f783492898ef186589e50c99ca060a19f14
GOyangi-7 <159999069+GOyangi-7@users.noreply.github.com>
2024-03-02 17:16:11 +0530

    Added sum.c file

---
+++ sum.c
@@ -0,0 +1,100 @@
+#include <stdio.h> 
+int main()
+{
+    //to calulate the sum of a and b variable 
+    int a , b ;
+    printf("enter a = ");
+    scanf("%d" , &a );
+
+    printf("enter b =");
+    scanf("%d" , &b );
+    //display the sum of a and b by using the formula a+b
+    printf("sum = %d" , a+b );
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

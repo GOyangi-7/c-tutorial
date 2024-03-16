2b3c44c387113315a1d6b9e116474a137ed6dade
GOyangi-7 <159999069+GOyangi-7@users.noreply.github.com>
2024-03-16 15:11:30 +0530

    Added sum.c file

--- square.c
+++ square.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main()
 {
+    //calculate the area of square
     int area_of_square ;
     printf("area : ");
     scanf(" area : %d", &area_of_square);
+    //it will show the area of square with the help of the formula
     printf("area of square = %d",area_of_square*area_of_square );
     return 0 ;
     

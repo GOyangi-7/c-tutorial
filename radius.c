9afc4e423936841fecb5094b5cd83bcddece204c
GOyangi-7 <159999069+GOyangi-7@users.noreply.github.com>
2024-03-16 15:50:35 +0530

    Added sum.c file

---
+++ radius.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float radius ;
+    float pi = 3.14 ;
+    printf("enter radius = "); 
+    scanf("%f" , &radius);
+
+    printf("area of circle = %f " , pi*radius*radius );
+
+}

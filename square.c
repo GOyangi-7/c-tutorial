593967b4a5a3f14408c8d37d06ecf41f744a11b1
GOyangi-7 <159999069+GOyangi-7@users.noreply.github.com>
2024-03-16 15:04:30 +0530

    Added sum.c file

---
+++ square.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int area_of_square ;
+    printf("area : ");
+    scanf(" area : %d", &area_of_square);
+    printf("area of square = %d",area_of_square*area_of_square );
+    return 0 ;
+    
+
+
+}

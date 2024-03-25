22fc13ff9d74d7889828caf31503191c936984c5
GOyangi-7 <159999069+GOyangi-7@users.noreply.github.com>
2024-03-25 13:48:26 +0530

    Added sum.c file

---
+++ case.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int lenght_is  , breath_is ;
+   printf("lenght is  ");
+   scanf("lenght is %d",&lenght_is );
+
+   printf("width is  ");
+   scanf("width is %d",&breath_is );
+
+   printf("perimater of rectangle = %d", 2*(lenght_is+breath_is) );
+   return 0;
+   
+  
+    
+}
--- square.c
+++ square.c
@@ -4,7 +4,7 @@ int main()
     //calculate the area of square
     int area_of_square ;
     printf("area : ");
-    scanf(" area : %d", &area_of_square);
+    scanf("%d", &area_of_square);
     //it will show the area of square with the help of the formula
     printf("area of square = %d",area_of_square*area_of_square );
     return 0 ;

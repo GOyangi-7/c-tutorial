f4eed33806e144ccf386a6752779f6b84087ca4d
GOyangi-7 <159999069+GOyangi-7@users.noreply.github.com>
2024-03-25 17:43:05 +0530

    Added sum.c file

--- divisable.c
+++ divisable.c
@@ -2,10 +2,14 @@
 int main()
 {
     //program to find if the number is divisable by 2 or not 
+    
     int a ;
     printf("enter number = ");
     scanf("%d",&a );
-    //in c true = 1 and false = 0  
+
+    /*in c true = 1 and false = 0  , here we used two operators 
+     %(arithmetic operator) and then ==0(relational opertor)*/    
+
     printf("%d", a%2==0 );
     return 0;
 
